b6b3e0a44e178a56d48efdfbffe928caf64606b6
BettiNAaaaa <bleilbettina72@gmail.com>
2025-11-13 19:49:44 +0100

    első kommit

---
+++ src/dao/adatbazis.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+    
+public class adatbazis {
+    
+    private static final String URL = "ajdfbajofb"; 
+    private static final String USER = "root"; 
+    private static final String PASSWORD = ""; 
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
+    
+
