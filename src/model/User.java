19c18131027b65c630b58d4d2097410a0ac85746
Betti <bleilbettina72@gmail.com>
2026-01-18 11:10:33 +0100

    rétegek redone

--- src/model/User.java
+++ src/model/User.java
@@ -5,55 +5,19 @@
 package model;
 
 
- 
 public class User {
-    private int id;
-    private String username;
-    private String passwordHash;
-    private boolean isAdmin;
+private int id;
+private String name;
+private String email;
 
-    public User() {}
 
-    public User(int id, String username, String passwordHash, boolean isAdmin) {
-        this.id = id;
-        this.username = username;
-        this.passwordHash = passwordHash;
-        this.isAdmin = isAdmin;
-    }
-
-    public User(int aInt, String string, String string0, String string1, String string2) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    // Getters/Setters
-
-    public String getPasswordHash() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public String getUsername() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public boolean isAdmin() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public String getName() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public String getEmail() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public String getPassword() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+public User(int id, String name, String email) {
+this.id = id;
+this.name = name;
+this.email = email;
 }
-    
-    
 
-    
-   
+
+public String getName() { return name; }
+}
 
