bcd939a9fc898883eec84967dfce6272632a54a9
Betti <bleilbettina72@gmail.com>
2026-02-22 18:22:55 +0100

    kesz?

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,5 +5,12 @@ import server.RestServer;
 public class Main {
     public static void main(String[] args) throws Exception {
         RestServer.main(args);
+        
+        
+        try (var c = dao.Database.getConnection()) {
+    System.out.println("DB: " + c.getCatalog());
+}
     }
+    
+
 }
--- src/dao/Database.java
+++ src/dao/Database.java
@@ -8,17 +8,11 @@ import java.sql.SQLException;
 
 public class Database {
 
-   
-    private static final String DB = "vizsgaremek%20%28ticketz%29";
-
-    
-    private static final int PORT = 3306;
-
     private static final String URL =
-            "jdbc:mysql://localhost:" + PORT + "/" + DB + "?useSSL=false&serverTimezone=UTC";
+        "jdbc:mysql://127.0.0.1:3306/vizsgaremekticketz"+ "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
 
     private static final String USER = "root";
-    private static final String PASSWORD = "root"; 
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
